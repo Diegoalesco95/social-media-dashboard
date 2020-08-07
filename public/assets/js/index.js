ed85dc2315a7df86f180d3cd02751c99bce7554a
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-08-07 17:18:52 -0500

    Animations & footer

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,1 @@
-"use strict";const checkbox=document.querySelector("input[type=checkbox]"),handleTheme=()=>{console.log("Change"),checkbox.checked?document.documentElement.setAttribute("data-theme","dark"):document.documentElement.setAttribute("data-theme","light")};checkbox.addEventListener("change",handleTheme);
+"use strict";const checkbox=document.querySelector("input[type=checkbox]"),handleTheme=()=>{checkbox.checked?document.documentElement.setAttribute("data-theme","dark"):document.documentElement.setAttribute("data-theme","light")};checkbox.addEventListener("change",handleTheme);
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 const checkbox = document.querySelector('input[type=checkbox]');
 
 const handleTheme = () => {
-  console.log('Change');
   if (checkbox.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
   } else {
